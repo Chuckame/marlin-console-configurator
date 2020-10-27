7d565452ed464c8033ac496578ccdc0ff2227ce0
Chuckame <chuckame@gmail.com>
2020-10-27 13:13:34 +0100

    Add comments into test file

--- src/test/resources/file.h
+++ src/test/resources/file.h
@@ -1,11 +1,11 @@
-#define CONSTANT_WITH_VALUE_STRING "a string value that requires quotes"
-#define CONSTANT_WITH_VALUE_COMPLEX_STRING "a complex value that requires quotes and\nreturns"
-#define CONSTANT_WITHOUT_VALUE
-#define WeiRd_conStANt hey
+#define CONSTANT_WITH_VALUE_STRING "a string value that requires quotes"   // hello
+#define CONSTANT_WITH_VALUE_COMPLEX_STRING "a complex value that requires quotes and\nreturns"//
+#define CONSTANT_WITHOUT_VALUE // another comment
+#define WeiRd_conStANt hey//comment
 
-//#define DISABLED_CONSTANT
+//#define DISABLED_CONSTANT // cool thing on disabled comment
 
-#define CONSTANT_WITH_VALUE_ARRAY { 14, 15, 28 }
+#define CONSTANT_WITH_VALUE_ARRAY { 14, 15, 28 }  // a comment
 #define CONSTANT_WITH_VALUE_NUMBER 1
 
 //#define DiSaBLeD_CoNsTANt2
